5cdee68a7c32b6e8c1e1a7df3bf43618c97608e4
vti <vti@clarive.com>
2015-08-31 11:02:51 +0200

    configure cert and key files

--- main.c
+++ main.c
@@ -48,8 +48,12 @@ clax_http_response_t response;
 typedef struct {
     char entropy_file[255];
     char log_file[255];
-    char no_ssl;
 
+    char no_ssl;
+    char cert_file[255];
+    char key_file[255];
+
+    /* private */
     FILE *_log_file;
 } opt;
 
@@ -63,13 +67,20 @@ void usage()
     fprintf(stderr,
             "usage: clax [-n] [-l log_file] [-e entropy_file]\n\n"
             "Options:\n\n"
+            "   common\n"
+            "   ------\n"
+            "   -l <log_file>      path to log file\n"
+            "\n"
+            "   ssl\n"
+            "   ---\n"
             "   -n                 do not use ssl\n"
             "   -e <entropy_file>  path to entropy file\n"
-            "   -l <log_file>      path to log file\n"
+            "   -t <cert_file>     path to cert file (CA included)\n"
+            "   -p <key_file>      path to private key file\n"
             "\n"
             );
 
-    abort();
+    exit(255);
 }
 
 void _exit(int code)
@@ -98,13 +109,15 @@ void clax_parse_options(opt *options, int argc, char **argv)
 {
     int c;
 
+    memset(options, 0, sizeof(opt));
+
 #ifdef MVS
     if (argc > 1)
         __argvtoascii_a(argc, (char **)argv);
 #endif
 
     opterr = 0;
-    while ((c = getopt (argc, argv, "hnl:e:")) != -1) {
+    while ((c = getopt(argc, argv, "hnl:e:t:p:")) != -1) {
         switch (c) {
         case 'l':
             strncpy(options->log_file, optarg, sizeof(options->log_file));
@@ -115,11 +128,25 @@ void clax_parse_options(opt *options, int argc, char **argv)
         case 'e':
             strncpy(options->entropy_file, optarg, sizeof(options->entropy_file));
             break;
+        case 't':
+            strncpy(options->cert_file, optarg, sizeof(options->cert_file));
+            break;
+        case 'p':
+            strncpy(options->key_file, optarg, sizeof(options->key_file));
+            break;
         case '?':
         case 'h':
-            usage();
-            break;
         default:
+            usage();
+        }
+    }
+
+    if (!options->no_ssl) {
+        if (!strlen(options->cert_file) || !strlen(options->key_file)) {
+            fprintf(stderr,
+                    "Error: cert_file and key_file are required\n\n"
+                   );
+
             usage();
         }
     }
@@ -139,7 +166,7 @@ int clax_recv(void *ctx,  unsigned char *buf, size_t len)
     return ret;
 }
 
-int clax_send( void *ctx, const unsigned char *buf, size_t len )
+int clax_send(void *ctx, const unsigned char *buf, size_t len)
 {
     int ret;
     int fd = fileno(stdout);
@@ -170,14 +197,14 @@ int clax_recv_ssl(void *ctx, unsigned char *buf, size_t len)
     return ret;
 }
 
-int clax_send_ssl( void *ctx, const unsigned char *buf, size_t len )
+int clax_send_ssl(void *ctx, const unsigned char *buf, size_t len)
 {
     int ret;
     mbedtls_ssl_context *ssl = ctx;
 
-    while ((ret = mbedtls_ssl_write(ssl, buf, len)) <= 0 ) {
+    while ((ret = mbedtls_ssl_write(ssl, buf, len)) <= 0) {
         if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-            clax_log("failed\n  ! mbedtls_ssl_write returned %d", ret );
+            clax_log("failed\n  ! mbedtls_ssl_write returned %d", ret);
             return ret;
         }
     }
@@ -187,8 +214,8 @@ int clax_send_ssl( void *ctx, const unsigned char *buf, size_t len )
     return ret;
 }
 
-int dev_random_entropy_poll( void *data, unsigned char *output,
-                             size_t len, size_t *olen )
+int dev_random_entropy_poll(void *data, unsigned char *output,
+                             size_t len, size_t *olen)
 {
     FILE *file;
     size_t ret, left = len;
@@ -197,26 +224,26 @@ int dev_random_entropy_poll( void *data, unsigned char *output,
 
     *olen = 0;
 
-    file = fopen( options.entropy_file, "rb" );
-    if ( file == NULL )
-        return( MBEDTLS_ERR_ENTROPY_SOURCE_FAILED );
+    file = fopen(options.entropy_file, "rb");
+    if (file == NULL)
+        return MBEDTLS_ERR_ENTROPY_SOURCE_FAILED;
 
-    while ( left > 0 ) {
+    while (left > 0) {
         /* /dev/random can return much less than requested. If so, try again */
-        ret = fread( p, 1, left, file );
-        if ( ret == 0 && ferror( file ) ) {
-            fclose( file );
-            return( MBEDTLS_ERR_ENTROPY_SOURCE_FAILED );
+        ret = fread(p, 1, left, file);
+        if (ret == 0 && ferror(file)) {
+            fclose(file);
+            return MBEDTLS_ERR_ENTROPY_SOURCE_FAILED;
         }
 
         p += ret;
         left -= ret;
-        sleep( 1 );
+        sleep(1);
     }
-    fclose( file );
+    fclose(file);
     *olen = len;
 
-    return( 0 );
+    return 0;
 }
 
 #define TRY if ((
@@ -375,45 +402,38 @@ void clax_loop_ssl()
     mbedtls_pk_context pkey;
     mbedtls_ssl_cache_context cache;
 
-    mbedtls_ssl_init( &ssl );
-    mbedtls_ssl_config_init( &conf );
-    mbedtls_ssl_cache_init( &cache );
-    mbedtls_x509_crt_init( &srvcert );
-    mbedtls_pk_init( &pkey );
-    mbedtls_entropy_init( &entropy );
-    mbedtls_ctr_drbg_init( &ctr_drbg );
+    mbedtls_ssl_init(&ssl);
+    mbedtls_ssl_config_init(&conf);
+    mbedtls_ssl_cache_init(&cache);
+    mbedtls_x509_crt_init(&srvcert);
+    mbedtls_pk_init(&pkey);
+    mbedtls_entropy_init(&entropy);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
 
 #if defined(MBEDTLS_DEBUG_C)
-    mbedtls_debug_set_threshold( DEBUG_LEVEL );
+    mbedtls_debug_set_threshold(DEBUG_LEVEL);
 #endif
 
     /*
      * 1. Load the certificates and private RSA key
      */
-    clax_log("Loading the server cert. and key..." );
+    clax_log("Loading the server cert. and key...");
 
-    /*
-     * This demonstration program uses embedded test certificates.
-     * Instead, you may want to use mbedtls_x509_crt_parse_file() to read the
-     * server and CA certificates, as well as mbedtls_pk_parse_keyfile().
-     */
-    ret = mbedtls_x509_crt_parse( &srvcert, (const unsigned char *) mbedtls_test_srv_crt,
-                                  mbedtls_test_srv_crt_len );
-    if ( ret != 0 ) {
+    ret = mbedtls_x509_crt_parse_file(&srvcert, options.cert_file);
+    if (ret != 0) {
         clax_log("failed\n  !  mbedtls_x509_crt_parse returned %d", ret);
         goto exit;
     }
 
-    ret = mbedtls_x509_crt_parse( &srvcert, (const unsigned char *) mbedtls_test_cas_pem,
-                                  mbedtls_test_cas_pem_len );
-    if ( ret != 0 ) {
-        clax_log("failed\n  !  mbedtls_x509_crt_parse returned %d", ret);
-        goto exit;
-    }
+    /*ret = mbedtls_x509_crt_parse_file(&srvcert, (const unsigned char *) mbedtls_test_cas_pem,*/
+                                  /*mbedtls_test_cas_pem_len);*/
+    /*if (ret != 0) {*/
+        /*clax_log("failed\n  !  mbedtls_x509_crt_parse returned %d", ret);*/
+        /*goto exit;*/
+    /*}*/
 
-    ret =  mbedtls_pk_parse_key( &pkey, (const unsigned char *) mbedtls_test_srv_key,
-                                 mbedtls_test_srv_key_len, NULL, 0 );
-    if ( ret != 0 ) {
+    ret =  mbedtls_pk_parse_keyfile(&pkey, (const unsigned char *) options.key_file, NULL);
+    if (ret != 0) {
         clax_log("failed\n  !  mbedtls_pk_parse_key returned %d", ret);
         goto exit;
     }
@@ -421,11 +441,11 @@ void clax_loop_ssl()
     clax_log("ok");
 
     if (options.entropy_file[0]) {
-        clax_log("Using '%s' as entropy file...", options.entropy_file );
+        clax_log("Using '%s' as entropy file...", options.entropy_file);
 
-        if ( ( ret = mbedtls_entropy_add_source( &entropy, dev_random_entropy_poll,
+        if ((ret = mbedtls_entropy_add_source(&entropy, dev_random_entropy_poll,
                                                 NULL, DEV_RANDOM_THRESHOLD,
-                                                MBEDTLS_ENTROPY_SOURCE_STRONG ) ) != 0 ) {
+                                                MBEDTLS_ENTROPY_SOURCE_STRONG)) != 0) {
             clax_log("failed\n  ! mbedtls_entropy_add_source returned -0x%04x", -ret);
             goto exit;
         }
@@ -435,80 +455,77 @@ void clax_loop_ssl()
 
     clax_log("Seeding the random number generator...");
 
-    if ( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
+    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
                                        (const unsigned char *) pers,
-                                       strlen( pers ) ) ) != 0 ) {
+                                       strlen(pers))) != 0) {
         clax_log("failed\n  ! mbedtls_ctr_drbg_seed returned %d", ret);
         goto exit;
     }
 
     clax_log("ok");
 
-    /*
-     * 4. Setup stuff
-     */
     clax_log("Setting up the SSL data....");
 
-    if ( ( ret = mbedtls_ssl_config_defaults( &conf,
+    if ((ret = mbedtls_ssl_config_defaults(&conf,
                 MBEDTLS_SSL_IS_SERVER,
                 MBEDTLS_SSL_TRANSPORT_STREAM,
-                MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 ) {
+                MBEDTLS_SSL_PRESET_DEFAULT)) != 0) {
         clax_log("failed\n  ! mbedtls_ssl_config_defaults returned %d", ret);
         goto exit;
     }
 
-    mbedtls_ssl_conf_rng( &conf, mbedtls_ctr_drbg_random, &ctr_drbg );
+    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 
-    mbedtls_ssl_conf_session_cache( &conf, &cache,
+    mbedtls_ssl_conf_session_cache(&conf, &cache,
                                     mbedtls_ssl_cache_get,
-                                    mbedtls_ssl_cache_set );
+                                    mbedtls_ssl_cache_set);
 
-    mbedtls_ssl_conf_ca_chain( &conf, srvcert.next, NULL );
-    if ( ( ret = mbedtls_ssl_conf_own_cert( &conf, &srvcert, &pkey ) ) != 0 ) {
-        clax_log(" failed\n  ! mbedtls_ssl_conf_own_cert returned %d", ret );
+    mbedtls_ssl_conf_ca_chain(&conf, srvcert.next, NULL);
+    if ((ret = mbedtls_ssl_conf_own_cert(&conf, &srvcert, &pkey)) != 0) {
+        clax_log(" failed\n  ! mbedtls_ssl_conf_own_cert returned %d", ret);
         goto exit;
     }
 
-    if ( ( ret = mbedtls_ssl_setup( &ssl, &conf ) ) != 0 ) {
-        clax_log(" failed\n  ! mbedtls_ssl_setup returned %d", ret );
+    if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0) {
+        clax_log(" failed\n  ! mbedtls_ssl_setup returned %d", ret);
         goto exit;
     }
 
-    clax_log("ok" );
+    clax_log("ok");
 
-    mbedtls_ssl_session_reset( &ssl );
+    mbedtls_ssl_session_reset(&ssl);
 
-    mbedtls_ssl_set_bio( &ssl, NULL, clax_send, clax_recv, NULL );
+    mbedtls_ssl_set_bio(&ssl, NULL, clax_send, clax_recv, NULL);
 
-    clax_log("ok" );
+    clax_log("ok");
 
     /*
      * 5. Handshake
      */
-    clax_log("Performing the SSL/TLS handshake..." );
+    clax_log("Performing the SSL/TLS handshake...");
 
-    while ( ( ret = mbedtls_ssl_handshake( &ssl ) ) != 0 ) {
-        if ( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE ) {
-            clax_log("failed\n  ! mbedtls_ssl_handshake returned %d", ret );
+    while ((ret = mbedtls_ssl_handshake(&ssl)) != 0) {
+        if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+            clax_log("failed\n  ! mbedtls_ssl_handshake returned %d", ret);
             goto exit;
         }
     }
 
-    clax_log("ok" );
+    clax_log("ok");
 
     clax_loop(&ssl, clax_send_ssl, clax_recv_ssl);
 
-    clax_log("Closing the connection..." );
+    clax_log("Closing the connection...");
 
-    while ( ( ret = mbedtls_ssl_close_notify( &ssl ) ) < 0 ) {
-        if ( ret != MBEDTLS_ERR_SSL_WANT_READ &&
-                ret != MBEDTLS_ERR_SSL_WANT_WRITE ) {
-            clax_log("failed\n  ! mbedtls_ssl_close_notify returned %d", ret );
+    while ((ret = mbedtls_ssl_close_notify(&ssl)) < 0) {
+        if (ret != MBEDTLS_ERR_SSL_WANT_READ &&
+                ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+            clax_log("failed\n  ! mbedtls_ssl_close_notify returned %d", ret);
             goto exit;
         }
     }
 
-    clax_log("ok" );
+    clax_log("ok");
 
     ret = 0;
     goto exit;
@@ -517,20 +534,20 @@ exit:
     fflush(stdout);
 
 #ifdef MBEDTLS_ERROR_C
-    if ( ret != 0 ) {
+    if (ret != 0) {
         char error_buf[100];
-        mbedtls_strerror( ret, error_buf, 100 );
-        clax_log("Last error was: %d - %s", ret, error_buf );
+        mbedtls_strerror(ret, error_buf, 100);
+        clax_log("Last error was: %d - %s", ret, error_buf);
     }
 #endif
 
-    mbedtls_x509_crt_free( &srvcert );
-    mbedtls_pk_free( &pkey );
-    mbedtls_ssl_free( &ssl );
-    mbedtls_ssl_config_free( &conf );
-    mbedtls_ssl_cache_free( &cache );
-    mbedtls_ctr_drbg_free( &ctr_drbg );
-    mbedtls_entropy_free( &entropy );
+    mbedtls_x509_crt_free(&srvcert);
+    mbedtls_pk_free(&pkey);
+    mbedtls_ssl_free(&ssl);
+    mbedtls_ssl_config_free(&conf);
+    mbedtls_ssl_cache_free(&cache);
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
 }
 
 int main(int argc, char **argv)
