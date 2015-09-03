beba05f7c1b5ee5ec54d0ebedf7d6465cb23b7c2
vti <vti@clarive.com>
2015-09-03 16:16:55 +0200

    make testing framework more robust

--- tests/u.h
+++ tests/u.h
@@ -33,58 +33,74 @@ int u_tests;
 int u_tests_failed;
 int u_rv;
 
-#define ASSERT(code)                        \
-    u_local_tests++;                        \
-    if (code) {                             \
-        u_rv = 1;                           \
-                                            \
-        printf("ok %d\n", u_tests + u_local_tests);     \
-    }                                       \
-    else {                                  \
-        u_rv = 0;                           \
-        u_tests_failed++;                   \
+#define NOT_OK                                                              \
+        u_tests_failed++;                                                   \
         printf(COLOR_RED "not ok %d\n" COLOR_OFF, u_tests + u_local_tests); \
-        printf("# %s:%d\n", __FILE__, __LINE__); \
+        printf("# %s:%d\n", __FILE__, __LINE__);
+
+#define ASSERT(code)                                    \
+    u_local_tests++;                                    \
+    if (code) {                                         \
+        u_rv = 1;                                       \
+                                                        \
+        printf("ok %d\n", u_tests + u_local_tests);     \
+    }                                                   \
+    else {                                              \
+        u_rv = 0;                                       \
+        NOT_OK \
     }
 
-#define ASSERT_EQ(got, exp)             \
-    ASSERT(got == exp)                  \
-                                        \
-    if (!u_rv) {                        \
-        printf("# " #got ":\n");        \
-        printf("#   got: %d\n", got);  \
-        printf("#   exp: %d\n", exp);  \
-    }
-
-#define ASSERT_STR_EQ(got, exp)             \
-    if (got != NULL) {                      \
-        ASSERT(strcmp(got, exp) == 0)       \
+#define ASSERT_EQ(got, exp)                 \
+    do {                                    \
+        int i = (int)got;                   \
+        ASSERT(got == exp)                  \
                                             \
         if (!u_rv) {                        \
             printf("# " #got ":\n");        \
-            printf("#   got: '%s'\n", got); \
-            printf("#   exp: '%s'\n", exp); \
+            printf("#   got: %d\n", i);     \
+            printf("#   exp: %d\n", exp);   \
         }                                   \
-    } else {                                \
-        u_local_tests++;                        \
-        u_tests_failed++; \
-                            \
-        printf(COLOR_RED "not ok %d\n" COLOR_OFF, u_tests + u_local_tests); \
-        printf("# %s:%d\n", __FILE__, __LINE__); \
-                                                    \
-        printf("# " #got ":\n");            \
-        printf("#   got: NULL\n");     \
-        printf("#   exp: '%s'\n", exp);     \
-    }
+    } while (0);
 
-#define ASSERT_STRN_EQ(got, exp, len)         \
-    ASSERT(strncmp(got, exp, len) == 0)       \
-                                              \
-    if (!u_rv) {                              \
-        printf("# " #got ":\n");              \
-        printf("#   got: '%.*s'\n", len, got);\
-        printf("#   exp: '%s'\n", exp);       \
-    }
+#define ASSERT_STR_EQ(got, exp)                 \
+    do {                                        \
+        char *p = (char *)got;                  \
+        if (p != NULL) {                        \
+            ASSERT(strcmp(p, exp) == 0)         \
+                                                \
+            if (!u_rv) {                        \
+                printf("# " #got ":\n");        \
+                printf("#   got: '%s'\n", p);   \
+                printf("#   exp: '%s'\n", exp); \
+            }                                   \
+        } else {                                \
+            u_local_tests++;                    \
+            NOT_OK                              \
+            printf("# " #got ":\n");            \
+            printf("#   got: NULL\n");          \
+            printf("#   exp: '%s'\n", exp);     \
+        }                                       \
+    } while (0);
+
+#define ASSERT_STRN_EQ(got, exp, len)           \
+    do {                                        \
+        char *p = (char *)got;                  \
+        if (p != NULL) {                        \
+            ASSERT(strncmp(p, exp, len) == 0)   \
+                                                \
+            if (!u_rv) {                        \
+                printf("# " #got ":\n");        \
+                printf("#   got: '%s'\n", p);   \
+                printf("#   exp: '%s'\n", exp); \
+            }                                   \
+        } else {                                \
+            u_local_tests++;                    \
+            NOT_OK                              \
+            printf("# " #got ":\n");            \
+            printf("#   got: NULL\n");          \
+            printf("#   exp: '%s'\n", exp);     \
+        }                                       \
+    } while (0);
 
 #define ASSERT_BUF_EQ(got, exp, len)    \
     ASSERT(memcmp(got, exp, len) == 0)  \
