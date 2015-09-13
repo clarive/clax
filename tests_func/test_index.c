91d87ddfc3becaee9f5f933eec63faf1a319f592
vti <viacheslav.t@gmail.com>
2015-09-13 21:56:20 +0200

    more functional tests

--- tests_func/test_index.c
+++ tests_func/test_index.c
@@ -16,4 +16,44 @@ TEST_START(index_page)
 }
 TEST_END
 
+TEST_START(ping)
+{
+    int rcount;
+    char output[1024];
+
+    rcount = execute("../clax -n -r . -l /dev/null", "GET /ping\r\n\r\n", output, sizeof(output));
+
+    ASSERT(rcount > 0);
+    ASSERT(util_parse_http_response(output, rcount))
+    ASSERT_MATCHES(output, "200 OK")
+    ASSERT_MATCHES(output, "{\"message\":\"pong\"}")
+}
+TEST_END
+
+TEST_START(not found)
+{
+    int rcount;
+    char output[1024];
+
+    rcount = execute("../clax -n -r . -l /dev/null", "GET /unknown\r\n\r\n", output, sizeof(output));
+
+    ASSERT(rcount > 0);
+    ASSERT(util_parse_http_response(output, rcount))
+    ASSERT_MATCHES(output, "404 Not Found")
+}
+TEST_END
+
+TEST_START(method not allowed)
+{
+    int rcount;
+    char output[1024];
+
+    rcount = execute("../clax -n -r . -l /dev/null", "POST /\r\n\r\n", output, sizeof(output));
+
+    ASSERT(rcount > 0);
+    ASSERT(util_parse_http_response(output, rcount))
+    ASSERT_MATCHES(output, "405 Method Not Allowed")
+}
+TEST_END
+
 SUITE_END
