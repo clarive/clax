b2644a27d3ca762f2ab93dc81311267931df5b70
vti <vti@clarive.com>
2017-06-21 16:02:49 +0200

    add headers for size_t and va_list

--- contrib/snprintf/snprintf.h
+++ contrib/snprintf/snprintf.h
@@ -7,6 +7,8 @@
 #ifdef HAVE_SNPRINTF
 #include <stdio.h>
 #else
+#include <stddef.h>
+#include <stdarg.h>
 extern int snprintf(char *, size_t, const char *, /*args*/ ...);
 extern int vsnprintf(char *, size_t, const char *, va_list);
 #endif
